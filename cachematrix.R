024a7345cd2df5818fbc6f5c0e21bf550b691e94
aumerez <aumerez@gmail.com>
2018-05-30 07:36:54 -0400

    Assignment 2 Coursera commit 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+#   _           _                                  _   ____  
+#  /_\  ___ ___(_) __ _ _ __  _ __ ___   ___ _ __ | |_|___ \ 
+# //_\\/ __/ __| |/ _` | '_ \| '_ ` _ \ / _ \ '_ \| __| __) |
+#/  _  \__ \__ \ | (_| | | | | | | | | |  __/ | | | |_ / __/ 
+#\_/ \_/___/___/_|\__, |_| |_|_| |_| |_|\___|_| |_|\__|_____|
+#                 |___/                                      
 
-## Write a short comment describing this function
+
+## This function is responsible for taking the values 
+## of a matrix and assigning them to a variable. It 
+## has the possibility of saving the inverse of that 
+## matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  	
+  	minv <- NULL
+
+  	## The set function
+
+  	set <- function(y) {
+    	x <<- y
+    	minv <<- NULL
+  	}
+
+  	## The get function
+
+  	get <- function() x
+  	setinverse <- function(solve) minv <<- solve
+  	getinverse <- function() minv
+  	list(set = set, get = get,
+       	setinverse = setinverse,
+       	getinverse = getinverse)
 
 }
 
-
-## Write a short comment describing this function
+## This function takes the matrix stored by the 
+## function XXXXX and calculates the inverse of 
+## that matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    minv <- x$getinverse()
+
+  	if(!is.null(minv)) {
+    	message("getting cached data")
+    	return(m)
+  	}
+
+  	data <- x$get()
+  	minv <- solve(data, ...)
+  	x$setinverse(minv)
+  	minv
+
 }
